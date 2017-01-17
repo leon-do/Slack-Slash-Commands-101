cea5b41dfef1e637e566324fbf8203ea87d6fa4d
Leon Do <leondo123@gmail.com>
2017-01-17 13:14:53 -0500

    square 1

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function sendMessage(urlObject){
 
     slack.webhook({
      channel: urlObject.channel_name,
-      text: "hello you sss: " + Number(userCommand) + 22;                   // EDIT THIS LINE
+      text: "hello you sss: " + parseFloat(userCommand) + 22;                   // EDIT THIS LINE
     }, function(err, response) {
         if (err){
             console.log(err)
